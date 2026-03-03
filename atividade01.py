e28658f1499b27122d4f5da96ffb95f85452daf1
ciro gomemes <vkaio2022@gmail.com>
2026-03-03 08:33:18 -0300

    Add files via upload

---
+++ atividade01.py
@@ -0,0 +1,9 @@
+numero = float(input("Digite um número: "))
+numero2 = float(input("Digite um número: "))
+
+soma = numero + numero2
+
+produto = numero * numero2
+
+print(F"A soma de {numero} e {numero2} é {soma}")
+print(F"O produto de {numero} e {numero2} é {produto} ")
