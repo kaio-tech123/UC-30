4da3a867484d3349750f3c55a73e343d54009274
ciro gomemes <vkaio2022@gmail.com>
2026-03-04 15:52:41 -0300

    Add files via upload

---
+++ UC - ALGORITIMO (04-03-26)/atividade.py
@@ -0,0 +1,7 @@
+compras = ["Arroz", "Feijão", "Cuscuz", "Macarrão", "Detergente"]
+print(f"Lista inicial: {compras}")
+
+item = input("Qual item você quer adicionar a lista? ")
+compras.append(item)
+
+print(f"Lista atualizada: {compras}")
