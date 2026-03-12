66bc2ad7fb41c5277b682ff38141905552247d5d
ciro gomemes <vkaio2022@gmail.com>
2026-03-12 08:40:15 -0300

    Add files via upload

---
+++ UC - ALGORITIMO (12-03-26)/atividade.py
@@ -0,0 +1,12 @@
+def calcularSoma(nota1, nota2):
+    soma = nota1 + nota2
+    produto = nota1 * nota2
+
+    return soma, produto
+
+soma, produto = calcularSoma(30,70)
+
+print(f"O resultado da soma é: {soma}")
+print(f"O resultado da soma é: {produto}")
+
+     
