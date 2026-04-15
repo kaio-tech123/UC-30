ea2060d7fa251c7d350a088940d331124a8c368b
ciro gomemes <vkaio2022@gmail.com>
2026-04-15 15:53:32 -0300

    Add files via upload

---
+++ UC - ALGORITIMO (15-04-26)/Q1.py
@@ -0,0 +1,15 @@
+def calcular_media():
+    soma_notas = 0
+    try:
+       
+        for i in range(3):
+            nota = float(input("Digite uma nota: "))
+            soma_notas += nota
+    
+        media = soma_notas / 3
+        print(f"Média final: {media:.2f}")
+
+    except ValueError:
+        print("As notas devem ser numéricas e encerrar a execução.")
+
+calcular_media()
